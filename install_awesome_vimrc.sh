1d57b664af6c13259c7283a61c9d33c685e45ee6
Papa Sall <papa@alphasall.com>
2016-12-26 10:26:50 +0100

    Update to use pach.vim

--- install_awesome_vimrc.sh
+++ install_awesome_vimrc.sh
@@ -4,12 +4,14 @@ echo 'set runtimepath+=~/.vim_runtime
 
 source ~/.vim_runtime/vimrcs/basic.vim
 source ~/.vim_runtime/vimrcs/filetypes.vim
-source ~/.vim_runtime/vimrcs/plugins_config.vim
-source ~/.vim_runtime/vimrcs/extended.vim
 
 try
-source ~/.vim_runtime/my_configs.vim
+source ~/Projects/config/pach.vim
 catch
-endtry' > ~/.vimrc
+endtry
+
+"source ~/.vim_runtime/vimrcs/plugins_config.vim
+source ~/.vim_runtime/vimrcs/extended.vim
+' > ~/.vimrc
 
 echo "Installed the Ultimate Vim configuration successfully! Enjoy :-)"
